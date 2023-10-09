9282d7b7d1ed6691a9fa7e592ab44b3ec7b2c21f
valomay <valomay@protonmail.com>
2023-10-09 15:23:36 +0200

    up rtl to ltl

--- lib/transformation/rtl2ltl.ml
+++ lib/transformation/rtl2ltl.ml
@@ -1,7 +1,7 @@
 open Lang
 open Lang.Ltl
 
-type reg = Reg of Lang.Mips.register | Spill
+type reg = Reg of Lang.Mips.register | Spill of int
 
 let tr_function (def : Lang.Rtl.pseudo_reg Lang.Rtl.function_def) =
   let code = Hashtbl.create 16 in
@@ -28,7 +28,7 @@ let tr_function (def : Lang.Rtl.pseudo_reg Lang.Rtl.function_def) =
     | _ ->
       match Hashtbl.find color r with
       | Regalloc.Spill _ -> failwith "TODO\n" 
-      | Regalloc.Reg r -> Reg r
+      | Regalloc.Reg r   -> Reg r
   in
 
   let rec tr_instruction i =
@@ -36,35 +36,63 @@ let tr_function (def : Lang.Rtl.pseudo_reg Lang.Rtl.function_def) =
       let inst = Hashtbl.find def.code i in
       Hashtbl.remove def.code i;
       match inst with
-      | Rtl.INop n   -> ignore (tr_instruction n); push_node i (INop n)
-      | Rtl.IGoto n  -> ignore (tr_instruction n); push_node i (IGoto n)
-      | Rtl.IPutchar (r, n) ->
-        let dest = tr_instruction n in
-        (match get_reg r with
-        | Reg r -> push_node i (IPutchar (r, dest))
-        | Spill -> failwith "TODO\n")
-      | Rtl.IMove (r1, r2, n)  -> tr_move i r1 r2 (tr_instruction n)
-      | Rtl.IOp (op, lr, r, n) -> tr_op i op lr r n
-      | Rtl.ILoad  (_a, _r, _n) -> assert false
-      | Rtl.IStore (_a, _r, _n) -> assert false
-      | Rtl.IPush (_r, _n) -> assert false
-      | Rtl.IPop (_r, _n) -> assert false
-      | Rtl.ICall (id, _, i, n) -> push_node i (ICall (id, i, n))
-      | Rtl.ICond (_c, _lr, _nt, _nf) -> assert false
-      | Rtl.IReturn None -> push_node i (IReturn None)
+      | Rtl.INop n                -> push_node i (INop ((tr_instruction n)))
+      | Rtl.IGoto n               -> push_node i (IGoto (tr_instruction n))
+      | Rtl.ICall (id, _, i, n)   -> push_node i (ICall (id, i, n))
+      | Rtl.IReturn None          -> push_node i (IReturn None)
+      | Rtl.IPutchar (r, n)       -> tr_putchar i r (tr_instruction n)
+      | Rtl.IMove (r1, r2, n)     -> tr_move i r1 r2 (tr_instruction n)
+      | Rtl.IOp (op, lr, r, n)    -> tr_op i op lr r n
+      | Rtl.ILoad  (a, r, n)      -> tr_load i a r (tr_instruction n)
+      | Rtl.IStore (a, r, n)      -> tr_store i a r (tr_instruction n)
+      | Rtl.IPush (r, n)          -> tr_push i r (tr_instruction n)
+      | Rtl.IPop (r, n)           -> tr_pop i r (tr_instruction n)
+      | Rtl.ICond (c, lr, nt, nf) ->
+        tr_cond c lr (tr_instruction nt) (tr_instruction nf)
       | _ -> assert false
     else 1
+  and tr_load i a r dest =
+    (match get_reg r with
+    | Reg r   -> push_node i (ILoad (a, r, dest))
+    | Spill n -> push_node i (ILoad (a, Mips.t8, push_node i
+                             (IStore (Rtl.AddrStack n, Mips.t8, dest)))))
+  and tr_store i a r dest =
+    (match get_reg r with
+    | Reg r   -> push_node i (IStore (a, r, dest))
+    | Spill n -> push_node i (ILoad (Rtl.AddrStack n, Mips.t8, push_node i
+                             (IStore (a, Mips.t8, dest)))))
+  and tr_push i r dest =
+    (match get_reg r with
+    | Reg r   -> push_node i (IPush (r, dest))
+    | Spill n -> push_node i (ILoad (Rtl.AddrStack n, Mips.t8, push_node i
+                             (IPush (Mips.t8, dest)))))
+  and tr_pop i r dest =
+    (match get_reg r with
+    | Reg r   -> push_node i (IPop (r, dest))
+    | Spill n -> push_node i (IPop (Mips.t8, push_node i
+                             (IStore (Rtl.AddrStack n, Mips.t8, dest)))))
+  and tr_putchar i r dest=
+    (match get_reg r with
+    | Reg r -> push_node i (IPutchar (r, dest))
+    | Spill n -> push_node i (ILoad (Rtl.AddrStack n, Mips.t8, push_node i
+                             (IPutchar (Mips.t8, dest)))))
   and tr_move i r1 r2 dest =
     match get_reg r1, get_reg r2 with
     | Reg r1, Reg r2 ->
-      if r1 = r2
-      then dest
-      else push_node i (IMove (r1, r2, dest))
-    | Spill, Reg r2 -> push_node i (IPush (r2, dest))
-    | _, _ -> assert false
-  and tr_op _i _op _lr _r _dest =
+      if r1 = r2 then dest
+                 else push_node i (IMove (r1, r2, dest))
+    | Spill n, Reg r2  ->
+      push_node i (IStore (Rtl.AddrStack n, r2, push_node i
+                  (IPush (r2, dest))))
+    | Reg r,   Spill n ->
+      push_node i (IMove (r, Mips.t8, push_node i
+                  (ILoad (Rtl.AddrStack n, Mips.t8, dest))))
+    | Spill n1, Spill n2 ->
+      push_node i (IStore (Rtl.AddrStack n1, Mips.t8, push_node i
+                  (ILoad (Rtl.AddrStack n2, Mips.t8, dest))))
+  and tr_cond _c _lr _nt _nf =
     assert false
-  and _tr_cond _c _lr _nt _nf =
+  and tr_op _i _op _lr _r _dest =
     assert false
   in
 
