999e87943ac761704642562a7acb9bdf809962a3
valomay <valomay@protonmail.com>
2023-11-10 14:15:18 +0100

    clean typechecker

--- soya/lib/frontend/typecheck.ml
+++ soya/lib/frontend/typecheck.ml
@@ -8,13 +8,13 @@ let type_check (prog : location program) =
 
 (* utils functions  --------------------------------------------------------- *)
 
-  let get_class n loc =
+  let get_classe n loc =
     match Hashtbl.find_opt envc n with
     | Some c -> c
     | None   -> Error_soy.Error.undeclared_class loc n
   in
 
-  let get_class_name t loc =
+  let get_classe_name t loc =
     match t with
     | TClass s -> s
     | _ -> Error_soy.Error.not_class loc t
@@ -39,7 +39,7 @@ let type_check (prog : location program) =
     | None   -> Error_soy.Error.undeclared_var loc v
   in
 
-  let unsafe_get_class t =
+  let unsafe_get_classe t =
     match t with
     | TClass s -> Hashtbl.find envc s
     | _ -> assert false
@@ -48,9 +48,9 @@ let type_check (prog : location program) =
 
 (* type checker ------------------------------------------------------------- *)
   let instanceof c cexp loc =
-    let cexp = get_class cexp loc in
+    let cexp = get_classe cexp loc in
     let rec aux c =
-      let c = get_class c loc in
+      let c = get_classe c loc in
       if c.name = cexp.name then true
       else
         match c.parent with
@@ -81,7 +81,7 @@ let type_check (prog : location program) =
     | Instanceof (c, (i, _)) ->
       (match c.expr with
       | Var v ->
-        let c = unsafe_get_class c.annot in
+        let c = unsafe_get_classe c.annot in
         if not (instanceof c.name i loc) then
           (Env.add v (TClass i) envt), envf
         else envt, envf
@@ -91,57 +91,52 @@ let type_check (prog : location program) =
 
 (* AST typer ---------------------------------------------------------------- *)
 
-  let rec type_expr exp (expr : location expression) env : typ expression =
+  let rec type_expr expr env : typ expression =
     match expr.expr with
-    | Char c          -> check_type expr.annot TChar exp; mk_expr exp (Char c)
-    | Cst c           -> type_const exp expr.annot c
-    | Bool b          -> mk_expr exp (Bool b)
-    | Var v           -> type_var v expr.annot env
+    | Char c          -> mk_expr TChar (Char c)
+    | Cst c           -> mk_expr TInt  (Cst c)
+    | Bool b          -> mk_expr TBool (Bool b)
+    | Var v           -> mk_expr (get_var_type v expr.annot env) (Var v)
     | Binop ((Add | Mul | Sub as op), e1, e2) ->
-      let e1t = type_expr TInt e1 env in
-      let e2t = type_expr TInt e2 env in
+      let e1t = type_expr e1 env in
+      let e2t = type_expr e2 env in
       check_type e1.annot e1t.annot TInt; check_type e2.annot e2t.annot TInt;
       mk_expr TInt (Binop (op, e1t, e2t))
     | Binop (Lt as op, e1, e2) ->
-      let e1t = type_expr TInt e1 env in
-      let e2t = type_expr TInt e2 env in
+      let e1t = type_expr e1 env in
+      let e2t = type_expr e2 env in
       check_type e1.annot e1t.annot TInt; check_type e2.annot e2t.annot TInt;
       mk_expr TBool (Binop (op, e1t, e2t))
     | Binop ((And | Or as op), e1, e2) ->
-      let e1t = type_expr TBool e1 env in
-      let e2t = type_expr TBool e2 env in
+      let e1t = type_expr e1 env in
+      let e2t = type_expr e2 env in
       check_type e1.annot e1t.annot TBool; check_type e2.annot e2t.annot TBool;
       mk_expr TBool (Binop (op, e1t, e2t))
     | Instanceof (e, (c, a)) ->
-      let cn = (get_class c a).name in
-      let et = type_expr TVoid e env in
-      let _  = get_class_name et.annot e.annot in
+      let cn = (get_classe c a).name in
+      let et = type_expr e env in
+      (* check if e is a class *)
+      ignore (get_classe_name et.annot e.annot);
       mk_expr TBool (Instanceof (et, (c, TClass cn)))
-    | Call (f, args) -> type_function f args expr.annot env
+    | Call (f, args)     -> type_function f args expr.annot env
     | MCall (c, f, args) -> type_method c f args expr.annot env
-    | New (c, args) -> type_constructor c args expr.annot env
+    | New (c, args)      -> type_constructor c args expr.annot env
     | NewTab (t, s) ->
-      let st = type_expr TInt s env in
+      let st = type_expr s env in
+      check_type s.annot st.annot TInt;
       mk_expr (TArray t) (NewTab (t, st))
     | Read m  -> type_read m env
-    | This    -> mk_expr (type_var "this" expr.annot env).annot This
-    | Super   -> mk_expr (type_var "super" expr.annot env).annot Super
+    | This    -> mk_expr (get_var_type "this"  expr.annot env) (Var "this")
+    | Super   -> mk_expr (get_var_type "super" expr.annot env) (Var "this")
 
-  and type_var v loc env =
-    mk_expr (get_var_type v loc env) (Var v)
-
-  and type_const exp loc c =
-    match exp with
-    | TChar -> mk_expr exp (Char c)
-    | _     -> check_type loc TInt exp; mk_expr exp (Cst c)
 
   and type_args args (f : location function_def) loc env =
     try
       List.fold_left2 (fun n arg (_, t) ->
-          let e = type_expr t arg env in
+          let e = type_expr arg env in
           check_type arg.annot e.annot t;
-          e :: n
-        ) [] args f.params
+          e :: n)
+        [] args f.params
     with Invalid_argument _ ->
       Error_soy.Error.number_arguent loc
         (List.length f.params) (List.length args)
@@ -154,19 +149,19 @@ let type_check (prog : location program) =
   and type_method c f args loc env =
     match c.expr with
     | Var v when not (Env.mem v env) -> (* static method *)
-      let c = get_class v c.annot in
+      let c = get_classe v c.annot in
       let m = get_static_method envc v f loc in
       let args  = type_args args m loc env in
       mk_expr m.return (Call (f ^ "$" ^ c.name, args))
     | _ ->
-      let ct    = type_expr TVoid c env in
-      let s     = get_class_name ct.annot c.annot in
+      let ct    = type_expr c env in
+      let s     = get_classe_name ct.annot c.annot in
       let m     = get_method envc s f loc in
       let args  = type_args args m loc env in
       mk_expr m.return (MCall (ct, f, args))
 
   and type_constructor cn args loc env =
-    let c = get_class cn loc in
+    let c = get_classe cn loc in
     if c.abstract then Error_soy.Error.implement_abstract loc;
     let m = get_method envc cn "constructor" loc in
     let args = type_args args m loc env in
@@ -175,13 +170,14 @@ let type_check (prog : location program) =
   and type_read m env =
     match m with
     | Arr (a, i) ->
-      let at = type_expr TVoid a env in
+      let at = type_expr a env in
       let t  = get_array_type at.annot a.annot in
-      let it = type_expr TInt i env in
+      let it = type_expr i env in
+      check_type i.annot it.annot TInt;
       mk_expr (TArray t) (Read (Arr (at, it)))
     | Atr (c, f, floc) ->
-      let ct  = type_expr TVoid c env in
-      let s   = get_class_name ct.annot c.annot in
+      let ct  = type_expr c env in
+      let s   = get_classe_name ct.annot c.annot in
       let t   = get_field envc s f floc in
       mk_expr (snd t) (Read (Atr (ct, f, floc)))
   in
@@ -189,41 +185,43 @@ let type_check (prog : location program) =
   let rec type_instruction i (ret: typ) env : typ instruction =
     match i with
     | Putchar e ->
-      let et = type_expr TChar e env in
+      let et = type_expr e env in
+      check_type e.annot et.annot TChar;
       Putchar et
     | Set (s, sloc, e) ->
       let t  = get_var_type s sloc env in
-      let et = type_expr t e env in
+      let et = type_expr e env in
       check_type e.annot et.annot t;
       Set (s, sloc, et)
     | If (c, e1, e2) ->
-      let ct = type_expr TBool c env in
+      let ct = type_expr c env in
       check_type c.annot TBool ct.annot;
       let envt, envf = down_cast_condition ct c.annot (env, env) in
       If (ct, type_sequence e1 ret envt, type_sequence e2 ret envf)
     | While (c, e) ->
-      let ct = type_expr TBool c env in
+      let ct = type_expr c env in
       check_type c.annot TBool ct.annot;
       While (ct, type_sequence e ret env)
     | Return e ->
-      let et = type_expr ret e env in
+      let et = type_expr e env in
+      check_type e.annot et.annot ret;
       Return et
-    | Expr e -> Expr (type_expr TVoid e env)
+    | Expr e -> Expr (type_expr e env)
     | Write (m, e) ->
+      let et  = type_expr e env in
       match m with
       | Arr (a, i) ->
-        let it = type_expr TInt i env in
+        let it = type_expr i env in
         check_type i.annot it.annot TInt;
-        let at  = type_expr TVoid a env in
+        let at  = type_expr a env in
+        (* check if it's an array *)
         let t   = get_array_type at.annot a.annot in
-        let et  = type_expr t e env in
         check_type e.annot et.annot t;
         Write (Arr(at, it), et)
       | Atr (c, f, floc) ->
-        let ct    = type_expr TVoid c env in
-        let s     = get_class_name ct.annot c.annot in
-        let _, t  = get_field envc s f floc  in
-        let et    = type_expr t e env in
+        let ct= type_expr c env in
+        let s = get_classe_name ct.annot c.annot in
+        let t = snd (get_field envc s f floc)   in
         check_type e.annot et.annot t;
         (Write (Atr (ct, f, floc), et))
   and type_sequence s ret env =
@@ -268,7 +266,7 @@ let type_check (prog : location program) =
       match c.parent with
       | None -> env, c
       | Some (cn, loc) ->
-        let pc = get_class cn loc in
+        let pc = get_classe cn loc in
         let c = if pc.abstract then check_abstract pc c else c in
         Env.add "super" (TClass pc.name) env, c
     in
